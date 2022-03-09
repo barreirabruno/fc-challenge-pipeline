fce7d4cad699e4aef9c479b939300588a73eb9df
brunobarreira <brunobarreirasilva@gmail.com>
2022-03-08 23:17:16 -0300

    feat: add subtraction function

--- src/Calculator.ts
+++ src/Calculator.ts
@@ -7,4 +7,17 @@ export class Calculator {
 
         return result
     }
+
+    subtractionOperation(numbers: number[]):number {
+        let result: number = numbers[0]
+        numbers.map((number, index) => {
+            console.log('[INDEX]', index)
+            if(index === 1) {
+                console.log(result = result - number)
+                return result
+            }
+        })
+
+        return result
+    }
 }
