e9ccc3138a4b1de58f515279fbcb1f63266aea5b
brunobarreira <brunobarreirasilva@gmail.com>
2022-03-12 14:31:32 -0300

    refactor: remove call to method

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,3 @@ const numbersA = [5, 3]
 console.log("[CALCULATING]")
 console.log("[DONE]")
 console.log("[RESULT]: ", calculatorInstance.subtractionOperation(numbersA))
-
-
-console.log("[CONVERSATION BEING STARTED]]")
-console.log("[RESULT]: ", calculatorInstance.provideAGreeting("Bruno"))
