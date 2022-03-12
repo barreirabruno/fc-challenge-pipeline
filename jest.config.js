e6f1480e40b9df10d7ae83a41a3fe8a22ffd5b49
brunobarreira <brunobarreirasilva@gmail.com>
2022-03-12 02:09:55 -0300

    chore: stop collect coverage from index and dist

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  roots: [
+    '<rootDir>/src'
+  ],
+  collectCoverageFrom:
+    ['<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/__tests__',
+    '!<rootDir>/src/index.ts',
+      '**/dist/**'],
+  coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  preset: 'ts-jest',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
 };
