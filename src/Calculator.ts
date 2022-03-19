236b3e8ea61f3f395b745fa02f50493c9bdf2d2a
brunobarreira <brunobarreirasilva@gmail.com>
2022-03-19 18:12:00 -0300

    feat: add filter by referencePointId

--- src/Calculator.ts
+++ src/Calculator.ts
@@ -1,3 +1,18 @@
+interface ReferencePoint {
+    id: string
+    location: string
+    name: string
+    comment: string
+}
+
+interface Property {
+    rooms: number
+    bathroom: number
+    kitchen: number
+    elevator: string
+    referencePointArray: ReferencePoint[]
+}
+
 export class Calculator {
     sumOperation(numbers: number[]): number {
         let result: number = 0
@@ -22,5 +37,9 @@ export class Calculator {
 
     provideAGreeting(name: string): string {
         return `Hello ${name}, how are you doing?`
+    }
+
+    simpleFilter(listToFilter: Property[], referencePointParam: string): Property[] {
+        return listToFilter.filter(property => property.referencePointArray.some(rfp => rfp.id === referencePointParam))
     }
 }
